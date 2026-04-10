fd4cef3fc3cfb3aec696a8e59e662cf4d3382c56
tomal2915 <tomal2915@gmail.com>
2026-04-10 15:09:00 +0600

    clean start — removed all tracked files

--- ml/resave_model.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-print("Loading old model...")
-model = tf.keras.models.load_model('ml/bangla_ocr.h5')
-
-print("Saving in new .keras format...")
-model.save('ml/bangla_ocr.keras')
-
-print("Done. New file: ml/bangla_ocr.keras")
